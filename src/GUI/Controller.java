6c3c9eeac5eb595e062b6a25e0897ea9f083c79e
Dylan <dfmcvay@gmail.com>
2016-02-11 14:56:39 -0500

    GUI update - using JavaFX scene builder

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -1,4 +1,40 @@
+
 package GUI;
 
-public class Controller {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import javafx.scene.control.ListView;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button addButton;
+
+    @FXML
+    private TextField myTextField;
+
+    @FXML
+    private ListView<String> myListView;
+    private ObservableList<String> listViewData = FXCollections.observableArrayList();
+
+    @Override
+    public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+        addButton.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                listViewData.add(myTextField.getText());
+                myListView.setItems(listViewData);
+            }
+        });
+    }
 }
--- src/GUI/Main.java
+++ src/GUI/Main.java
@@ -1,44 +1,28 @@
 package GUI;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-public class Main extends Application implements EventHandler<ActionEvent> {
-
-    Button close;
+public class Main extends Application {
 
     public static void main(String[] args) {
-        launch(args);
+        Application.launch(Main.class, (java.lang.String[])null);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("gui.fxml"));
-        primaryStage.setTitle("Hello World");
-
-        close = new Button();
-        close.setText("Close");
-        close.setOnAction(this);
-
-        StackPane layout = new StackPane();
-        layout.getChildren().add(close);
-
-        primaryStage.setScene(new Scene(layout, 300, 275));
-
-        primaryStage.show();
-    }
-
-    @Override
-    public void handle(ActionEvent evt) {
-        if(evt.getSource() == close) {
-            System.exit(0);
+    public void start(Stage primaryStage) {
+        try {
+            Pane root = FXMLLoader.load(getClass().getResource("gui.fxml"));
+            Scene scene = new Scene(root);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception ex) {
+            Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 }
