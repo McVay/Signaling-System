b955b3e2df256f469a9fc7304bc3b9db1a610e14
Dylan McVay <dfmcvay@gmail.com>
2016-02-14 20:39:32 -0500

    Bound sliders to text boxes

--- src/GUI/Controller.java
+++ src/GUI/Controller.java
@@ -3,19 +3,44 @@ package GUI;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleButton;
+import javafx.util.StringConverter;
 
 public class Controller implements Initializable {
 
     @FXML
     private ToggleButton toggleSwitch;
 
+    @FXML
+    private Slider carNumSlider;
+    @FXML
+    private TextField carNumText;
+
+    @FXML
+    private Slider timeScaleSlider;
+    @FXML
+    private TextField timeScaleText;
+
+    @FXML
+    private Slider lightIntervalSlider;
+    @FXML
+    private TextField lightIntervalText;
+
     @Override
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
+
+        carNumText.textProperty().bindBidirectional(carNumSlider.valueProperty(), getStringConverter(carNumSlider,carNumText));
+        timeScaleText.textProperty().bindBidirectional(timeScaleSlider.valueProperty(), getStringConverter(timeScaleSlider, timeScaleText));
+        lightIntervalText.textProperty().bindBidirectional(lightIntervalSlider.valueProperty(), getStringConverter(lightIntervalSlider, lightIntervalText));
+
         toggleSwitch.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -28,5 +53,36 @@ public class Controller implements Initializable {
                 System.out.println("Simulation Status: " + toggleSwitch.isSelected());
             }
         });
+    }
+
+    public Boolean isNumber(String input) {
+        return new Scanner(input).hasNextInt();
+    }
+
+    public StringConverter<Number> getStringConverter(final Slider slider, final TextField textValue) {
+        return new StringConverter<Number>() {
+            @Override
+            public String toString(Number object) {
+                return Integer.toString(object.intValue());
+            }
+            @Override
+            public Number fromString(String string) {
+                double val;
+
+                if(isNumber(string)) {
+                    val = Double.parseDouble(string);
+
+                    if(val > slider.getMax())
+                        val = slider.getMax();
+                    if(val <  slider.getMin())
+                        val = slider.getMin();
+                }
+                else
+                    val = slider.getValue();
+
+                textValue.setText(Integer.toString((int)val));
+                return (int)val;
+            }
+        };
     }
 }
