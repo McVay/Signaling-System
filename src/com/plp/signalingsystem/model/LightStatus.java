9d0bd0023d0c6ced0f91fc4c72c9ccc8d4f57792
Dylan McVay <dfmcvay@gmail.com>
2016-03-03 11:32:32 -0500

    removed .html picture example.. Merging this to main

--- src/com/plp/signalingsystem/model/LightStatus.java
+++ src/com/plp/signalingsystem/model/LightStatus.java
@@ -2,5 +2,5 @@ package com.plp.signalingsystem.model;
 
 
 public enum LightStatus {
-    RED, YELLOW, GREEN, BLINKING_YELLOW, BLINKING_RED, LEFT_TURN
-}
+        RED, YELLOW, GREEN, BLINKING_YELLOW, BLINKING_RED, LEFT_TURN
+        }
